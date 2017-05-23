9051c33d13e60ea5f847253f83376f69b5a9be67
Fa-James1 <ftamimu@kippnyccp.org>
2017-05-23 11:11:19 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+  $('button').click(function(){
+    $('img').css('width','600px');
+  }):
+  
+  $(document).ready(function(){
+  $('button').click(function(){
+    $('p').css('color','red');
+  }):
